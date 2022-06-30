11df1392368db3e9af94c3fee006102cdc29f101
Yash-Varia <100341787+Yash-Varia@users.noreply.github.com>
2022-06-30 17:46:15 +0530

    Final Commit

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -36,11 +36,13 @@ public class ObjectManager : MonoBehaviour
     {
         MeshRenderer myRenderer = currentObject.GetComponent<MeshRenderer>();
 
-        float alpha = Mathf.InverseLerp(0f, 255f, myObject.transparency);
 
+        //Use this code to create new transparent material and apply it to the gameobject
+
+        /*
+        float alpha = Mathf.InverseLerp(0f, 255f, myObject.transparency);
+        
         Material mat = new Material(Shader.Find("Standard"));
-        mat.SetColor("_Color",Color.clear);
-        mat.SetColor("_Color", new Color(myObject.color1, myObject.color2, myObject.color3, alpha));
 
         mat.SetFloat("_Mode", 3);
         mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
@@ -51,7 +53,18 @@ public class ObjectManager : MonoBehaviour
         mat.DisableKeyword("_ALPHATEST_ON");
         mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
 
+        mat.SetColor("_Color", new Color(myObject.color1, myObject.color2, myObject.color3, alpha));
+
         myRenderer.material = mat;
+        */
+
+
+        // Use this code to set the color to the default opaque material of the gameobject
+        float alpha = Mathf.InverseLerp(0f, 255f, myObject.transparency);
+        Color myColor = new Color(myObject.color1, myObject.color2, myObject.color3, alpha);
+        myRenderer.material.color = myColor;
+
+        // while using one type of material code: comment out the other code [Use either trnsparent material code OR Opaque material code]
     }
 
     private void SetScale(GameObject currentObject, ObjectFields myObject)
