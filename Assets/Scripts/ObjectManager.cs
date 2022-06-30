ccf37b5e65a6012ec3559609a61339a6cb0a95a2
Yash-Varia <100341787+Yash-Varia@users.noreply.github.com>
2022-06-30 21:35:22 +0530

    Primitive Prefabs Created

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
-{
+{   
+    public GameObject PrimitiveCubePrefab;
+    public GameObject PrimitiveSpherePrefab;
+
     public void SpawnObject(ObjectFields myObject)
     {
-        PrimitiveType type = GetObjectType(myObject.Type);
-
-        GameObject currentObject =  GameObject.CreatePrimitive(type);
+        GameObject currentObject;
+        if (myObject.Type == "Cuboid" || myObject.Type == "Cube")
+        {
+            currentObject = Instantiate(PrimitiveCubePrefab);
+        }
+        else if (myObject.Type == "Sphere")
+        {
+            currentObject = Instantiate(PrimitiveSpherePrefab);
+        }
+        else 
+        {
+            currentObject = Instantiate(PrimitiveSpherePrefab);
+        }
 
         SetPosition(currentObject, myObject);
         SetScale(currentObject, myObject);
         SetColor(currentObject, myObject);
-    }
-
-    public PrimitiveType GetObjectType(string myType)
-    {
-        switch (myType)
-        {
-            case "Cuboid": case "Cube": return PrimitiveType.Cube;
-
-            case "Sphere": return PrimitiveType.Sphere;
-
-            default: return PrimitiveType.Cylinder;
-        }
     }
 
     private void SetPosition(GameObject currentObject, ObjectFields myObject)
@@ -38,8 +39,6 @@ public class ObjectManager : MonoBehaviour
 
 
         //Use this code to create new transparent material and apply it to the gameobject
-
-        /*
         float alpha = Mathf.InverseLerp(0f, 255f, myObject.transparency);
         
         Material mat = new Material(Shader.Find("Standard"));
@@ -56,15 +55,13 @@ public class ObjectManager : MonoBehaviour
         mat.SetColor("_Color", new Color(myObject.color1, myObject.color2, myObject.color3, alpha));
 
         myRenderer.material = mat;
-        */
-
 
         // Use this code to set the color to the default opaque material of the gameobject
-
+        /*
         float alpha = Mathf.InverseLerp(0f, 255f, myObject.transparency);
         Color myColor = new Color(myObject.color1, myObject.color2, myObject.color3, alpha);
         myRenderer.material.color = myColor;
-
+        */
 
         // while using one type of material code: comment out the other code [Use either trnsparent material code OR Opaque material code]
     }
